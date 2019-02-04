3168743853eea84db6f691e55349f3415c177042
Iralin <cmcmillen529@gmail.com>
2019-02-04 05:47:31 -0500

    Cleaning up
    Some old code was removed that's not being used anymore.

--- javascript/jqueryStuff.js
+++ javascript/jqueryStuff.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $(".held").hide(); //Hide all the text that shows when shift is held
+    $(".held").hide();
     $("#leftMapPanelInnerDiv").hide();
     $("#rightMapPanelInnerDiv").hide();
     $("#leftMapPanelInnerDivSpecial").show();
@@ -19,8 +19,6 @@ $(document).ready(function() {
     var loopCounter = 0;
 
     function gLoop() {
-        checkUpgrades(); //Function below. Updates display of upgrades
-        getStoryUpdates(); //Function below. Reveals new story bits.
         updateSoul();
         if(loopCounter >=10) {
             updatePlayerStats();
@@ -28,23 +26,10 @@ $(document).ready(function() {
         }
         loopCounter++;
     }
-    var gameLoop = setInterval(gLoop, 50); //get that loop goin', boi
+    var gameLoop = setInterval(gLoop, 50);
 
     function updateSoul() {
         $("#soulCount").html(formatNumber(player.soulTotal));
-    }
-
-    function checkUpgrades() {
-        for(var x in player.upgrades) {
-            if(player.upgrades.hasOwnProperty(x)) {
-                if(player.soulTotal >= player.upgrades[x].cost) {
-                    $("#"+player.upgrades[x].id).prop("disabled", false);
-                }
-                else {
-                    $("#"+player.upgrades[x].id).prop("disabled", true);
-                }
-            }
-        }
     }
 
     function updatePlayerStats() {
@@ -54,57 +39,7 @@ $(document).ready(function() {
             $("#playerHealthLabel").html(formatNumber(player.health) + " <b>|</b> " + formatNumber(player.health));
             $("#playerHealthBar").css("width", "100%");
         }
-        //SET VARIABLE FOR FIGHTING SO HEALTH ISNT UPDATED
     }
-
-    var story = { 
-        //Story-unlocking will go here. All story text will be in story tab, inside of individual <span> tags
-        storyIntro: {
-            category: "Tails", //Category. Must be capitalized, fills in tab and button name both
-            position: "st1", //id of <span> tag to be unhidden. ss1 stands for "story soul 1". tails 3 would be "st3".
-        },
-        soul100: {
-            category: "Soul",
-            position: "ss1",
-        },
-    };
-
-    function getStoryUpdates() {
-        if(player.story.start) { //This'd be the first time the game started. For now it was making sure this code worked.
-            newStoryUpdate(story.storyIntro); 
-            player.story.start = false;
-        }
-        if(player.story.soulStory1 && player.soulTotal >= 100) {
-            newStoryUpdate(story.soul100);
-            player.story.soulStory1 = false;
-        }
-    } //How the function will be called. Not sure if this is the method I want.
-      //May have it be more specific, only triggering once on certain things. Probably will, as this method will quickly bloat.
-
-    function newStoryUpdate(storyObj) {
-        $("#storyButton").addClass("newStry"); //Glow the story button
-        $("#story"+storyObj.category+"Button").addClass("newStry"); //glow the sub-tab button
-        $("#"+storyObj.position).css("display", "inline-block");
-    }
-
-    $("#storyButton").click(function() {
-        $(".storyTabCategories").each(function() {
-            //Essentially, this is: If story tab clicked and visible tab is soul, remove glow from soul.
-            //Function of this is to immediately remove the glow from the active tab, if it's there
-            //If a story condition is met while in soul tab, glow will remain until button is clicked, but im not sure how best
-            //to counteract that.
-            if($(this).css("display") != "none") {
-                var tabname = $(this).attr("id");
-                tabname = tabname.substring(0, (tabname.length - 3));
-                $("#"+tabname+"Button").removeClass("newStry");
-            }
-        });
-        $(this).removeClass("newStry");
-    });
-
-    $(".storyTabButtons").click(function() {
-        $(this).removeClass("newStry"); //Same as above, basically.
-    });
 
     //Hold shift to change texts
     $(document).bind('keydown',function(e) {
@@ -120,6 +55,7 @@ $(document).ready(function() {
         }
     });
 
+    //Could optimize this better. Have an activeTab variable, change active tab. Probably a good idea to do this.
     function updateTailsTabs() { //Refresh the tabs for the ascension thing
         $(".tailsInnerPanel").each(function() {
             if($(this).css("display") != "none") //If this tab is visible
@@ -364,58 +300,6 @@ $(document).ready(function() {
         consumeLoop = setInterval(cLoop, 10);
     }
     
-
-/* CONSUME/RELEASE OLD
-    $(".buttonConsume").click(function() {
-        var targetID = $(this).attr("id").charAt(8); //Which target. soulCons1 -> soulRel1, soulCons2 -> soulRel2, etc
-        var bar = "#soulBar" + targetID; //Sets vars as matching target id's
-        var rel = "#soulRel" + targetID;
-        var cons = "#" + $(this).attr("id");
-        var perc = "#barPerc" + targetID;
-
-        var sCurr = 0; //Current soul = 0
-        $(this).css("display","none"); //Hide cons
-        $(rel).css("display","inline-block"); //Show rel
-        $(bar).css("width","100%"); //Fill bar
-
-        var interv = setInterval(consFunc, player.spd); //Set that interval
-
-        function consFunc() {
-            if(Number($(bar).css("width").replace(/[^\d\.\-]/g, '')) <= 0 || player.stop == targetID) {
-                if(player.stop != targetID) {
-                    addSoul(player.cGain, "d");
-                }
-                $(bar).css("width","0%");
-                $(cons).css("display","inline-block");
-                $(rel).css("display","none");
-                clearInterval(interv);
-                player.stop = "";
-            }
-            else {
-                if(player.sRate <= (player.sCap - sCurr)) {
-                    addSoul(player.sRate, "l");
-                    sCurr += player.sRate;
-                }
-                else {
-                    addSoul((player.sCap - sCurr), "l");
-                    sCurr = player.sCap;
-                }
-
-                if((1-parseFloat(sCurr/player.sCap)) <= 0) {
-                    $(bar).css("width", 0);
-                }
-                else {
-                    $(bar).css("width", (100-parseFloat(sCurr/player.sCap)*100)+"%");
-                }
-                $(perc).html((parseFloat(player.sCap - sCurr).toFixed(1)) + "/" + player.sCap);
-            }
-        }
-    });
-
-    $(".buttonRelease").click(function() {
-        player.stop = $(this).attr("id").charAt(7);
-    });
- */
     function addSoul(amnt, type) {
         player.soulTotal += amnt; //adds untyped soul to current, run, and alltime
         player.runTotalS += amnt;
@@ -475,11 +359,6 @@ $(document).ready(function() {
         });
     });
 
-    $(".upgrade").click(function() { //Each button has to link to its own cost with its own function assignment
-        $(this).attr("data-unlocked", true); //Sets unlocked attribute to true, triggers CSS change
-        $(this).prop("disabled", true);
-        removeSoul(player.upgrades[$(this).attr("id")].cost);
-    });
     function removeSoul(x) {
         player.soulTotal -= x;
     }
@@ -536,19 +415,6 @@ $(document).ready(function() {
         soulDark: 0,
         soulLight: 0,
     
-        //Upgrade details
-        upgrades: {
-            s11: {
-                cost: 100,
-                id: "s11",
-                unlocked: false,
-            },
-            s21: {
-                cost: 5000,
-                id: "s21",
-                unlocked: false,
-            },
-        },
         story: {
             start: true,
             soulStory1: true,
