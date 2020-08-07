c1f39c6929ee1cf31ad4662323d4aa3cb8535df8
cm97878 <cmcmillen529@gmail.com>
2020-08-07 12:12:45 -0400

    Maintenance
    Just a handful of misc updates, committing as I'll be switching computers.

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
-var player = {
+let player = {
     currentSoul: {
         //currency
-        soulTotal: 0,
+        soulTotal: 0, //should always be sum of next two, may want to remove?
         soulDark: 0,
         soulLight: 0,
         
@@ -24,32 +24,84 @@ var player = {
     
     unlocks: {
         //tabs
-        tailsUnlocked: true,
+        tailsUnlocked: false,
         storyUnlocked: false,
         crystalsUnlocked: false,
         mansionUnlocked: false,
 
         //prestige mechanics
-        prestigeLightUnlocked: true,
-        prestigeDarkUnlocked: true,
+        prestigeLightUnlocked: false,
+        prestigeDarkUnlocked: false,
+    },
+
+    areaInfo: {
+        special: {
+            home: {
+                title: "Home",
+                description: "test",
+            },
+        },
+        general: {
+            trailNorth1: {
+                title: "Forest Trail",
+                description: "A worn trail winds east away through the underbrush from the den nestled in the cliffside.\
+                It seems well-traveled by the smaller creatures of the forest, and a good place to hunt.",
+                enemies: ["redSquirrel", "blackSquirrel"], //Array of enemy names that appear
+                chance: [20, 5], //%chance for each, could combine this + above into objects?
+                toKill: 1, //# needed to kill to unlock connected nodes
+                killed: 0, //# killed
+                mastered: false, //i think this was meant to be a check of whether connected nodes have been unlocked or not but can just do killed==toKill?
+                toUnlock: ["trailNorth2"], //nodes unlocked when completed
+            },
+            trailNorth2: {
+                title: "Forest Trail",
+                description: "As the vegetation thins out for a short ways, the path seems to split. To the west is the trail to the\
+                den, while there's a fork to the east - one winding north, towards the sound and smell of flowing water, and one\
+                drifting south, deeper into the forest. There seems to be a small patch in the brambles to the southwest, as well.",
+                enemies: ["redSquirrel", "blackSquirrel"],
+                chance: [10, 5],
+                toKill: 2,
+                killed: 0,
+                mastered: false,
+                toUnlock: ["trailNorthClearing1", "trailRiver1", "trailNorth3"],
+            },
+            trailNorthClearing1: {
+                title: "Forest Clearing",
+                description: "A small path through some brambles you could barely squeeze through lead into this small clearing.\
+            The sunlight trickles through the thinner branches of the trees, giving the clearing a calm, serene look.",
+                enemies: ["redSquirrel", "blackSquirrel"],
+                chance: [5, 10],
+                toKill: 1,
+                killed: 0,
+                mastered: false,
+                toUnlock: [],
+            },
+        },
+        unlockedNodes: {
+            trailNorth1: true,
+            trailNorth2: true,
+        }
     },
 
     activeStuff: {
+        //whatever panels or tabs are currently open, may remove this from player object later
         activeMainTab: "soul",
         activeSoulTab: "map",
-        sidePanelSpecial: false,
-        sidePanelName: "",
-        leftPanelTitle: "",
-        rightPanelDescription: "",
+        sidePanelSpecial: "home",
+        sidePanelTitle: "error! report this",
+        sidePanelDescription: "error! report this",
 
-        isFighting: true,
+        isFighting: false, //a few tab changes and the like work differently when the player is mid-combat
     },
 
     loading: true,
     showIntro: true,
 }
 
-var nodes = new vis.DataSet([
+
+
+
+let nodes = new vis.DataSet([
     {
         id: 'home',  
         x: 0, 
@@ -57,10 +109,6 @@ var nodes = new vis.DataSet([
         group: 'forest',
         shape: 'hexagon',
         size: 15,
-        details: {
-            name: 'Home',
-            description: 'a'
-        }
     }, //home
 
 
@@ -70,17 +118,6 @@ var nodes = new vis.DataSet([
         x: 100, 
         y: -30,
         group: 'forest',
-        details: {
-            description: "A worn trail winds east away through the underbrush from the den nestled in the cliffside.\
-            It seems well-traveled by the smaller creatures of the forest, and a good place to hunt.",
-            enemies: ["redSquirrel", "blackSquirrel"],
-            chance: [20, 5],
-            toKill: 1,
-            killed: 0,
-            mastered: false,
-            toUnlock: ["trailNorth2"],
-            name: 'Forest Trail'
-        }
     }, //trailNorth1
             
     {
@@ -89,18 +126,6 @@ var nodes = new vis.DataSet([
         y: 10,
         group: 'forest',
         hidden: true,
-        details: {
-            description: "As the vegetation thins out for a short ways, the path seems to split. To the west is the trail to the\
-            den, while there's a fork to the east - one winding north, towards the sound and smell of flowing water, and one\
-            drifting south, deeper into the forest. There seems to be a small patch in the brambles to the southwest, as well.",
-            enemies: ["redSquirrel", "blackSquirrel"],
-            chance: [10, 5],
-            toKill: 2,
-            killed: 0,
-            mastered: false,
-            toUnlock: ["trailNorthClearing1", "trailRiver1", "trailNorth3"],
-            name: 'Forest Trail'
-        }
     }, //trailNorth2
 
     {
@@ -109,17 +134,6 @@ var nodes = new vis.DataSet([
         y: 40,
         group: 'forest',
         hidden: true,
-        details: {
-            description: "A small path through some brambles you could barely squeeze through lead into this small clearing.\
-            The sunlight trickles through the thinner branches of the trees, giving the clearing a calm, serene look.",
-            enemies: ["redSquirrel", "blackSquirrel"],
-            chance: [5, 10],
-            toKill: 1,
-            killed: 0,
-            mastered: false,
-            toUnlock: [],
-            name: 'Forest Clearing'
-        }
     }, //trailNorthClearing1
 
     {
@@ -181,7 +195,7 @@ var nodes = new vis.DataSet([
     }, //trailSouth1
 ]);
 
-var edges = new vis.DataSet([
+let edges = new vis.DataSet([
     {id: "home-trailNorth1", 
         from: 'home', to: 'trailNorth1'}, //home-trailNorth1
 
@@ -202,7 +216,7 @@ var edges = new vis.DataSet([
         from: 'home', to: 'trailSouth1'}, //home-trailSouth1
 ]);
 
-var options = {
+let options = {
     groups: {
         forest: {
             color: {
@@ -232,6 +246,7 @@ window.addEventListener('load', function() {
                 edges: edges,
                 options: options,
                 container: '',
+                unlockedNodes: player.areaInfo.unlockedNodes,
             }
         },
     
@@ -258,18 +273,16 @@ window.addEventListener('load', function() {
 
             this.network.on("click", (params) => {
                 const nodeID = params['nodes']['0'];
-
-                if (nodeID) { //returns true if a node was clicked
-                    const clickedNode = nodes.get(nodeID); //Gets the actual parameters of the node
-
-                    vm.updateSidePanels(clickedNode.shape == "hexagon", nodeID);
+                if (nodeID) {
+                    const clickedNode = nodes.get(nodeID);
+                    vm.updateSidePanels(clickedNode.shape == "hexagon", nodeID); //Maybe change to an event?
                 }
             });
-        }
+        },
     });
 
 
-    var vm = new Vue({
+    const vm = new Vue({
         el: '#vueWrapper',
         data: player,
         computed: {
@@ -296,11 +309,34 @@ window.addEventListener('load', function() {
                 }
                 else {
                     this.activeStuff.sidePanelSpecial = false;
+                    this.activeStuff.sidePanelTitle = this.areaInfo.general[name].title;
                 }
             },
+            saveToLocal: function () {
+                localStorage.setItem("manualSave", window.btoa(JSON.stringify(player)));
+                console.log("Saved:");
+                console.log(JSON.parse(window.atob(localStorage.getItem("manualSave"))));
+            },
+            loadFromLocal: function () {
+                try {
+                    let temp = JSON.parse(window.atob(localStorage.getItem("manualSave")));
+                    this.currentSoul = temp.currentSoul;
+                    this.stats = temp.stats;
+                    this.unlocks = temp.unlocks;
+                    this.areaInfo = temp.areaInfo;
+                    this.$forceUpdate();
+                }
+                catch(e) {
+                    console.log("No save found!");
+                }
+            },
+            clearData: function () {
+                localStorage.removeItem("manualSave");
+                console.log("Cleared save.");
+            }
         },
     });
-    player.loading = false;
+    player.loading = false; //TODO: This isn't good enough, as with components and the eventual loading in of data, it needs multiple. Semaphores seem to be suggested?
 });
 
 
